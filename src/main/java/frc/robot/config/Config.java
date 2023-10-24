d177d4d2d0fb2bb4c0b8474a64e2efeb659f6e86
fcuellar13 <105395555+fcuellar13@users.noreply.github.com>
2023-10-24 16:59:51 -0700

    Add wrist subsystem Closes #1

--- src/main/java/frc/robot/config/Config.java
+++ src/main/java/frc/robot/config/Config.java
@@ -14,8 +14,6 @@ import frc.robot.swerve.SwerveCorner;
 import frc.robot.swerve.SwerveModuleConstants;
 
 public class Config {
-  private static final String TYKE_SERIAL_NUMBER = "031617f6";
-
   public static final String SERIAL_NUMBER = System.getenv("serialnum");
   public static final boolean IS_DEVELOPMENT = true;
 
@@ -27,7 +25,7 @@ public class Config {
   public static final String CANIVORE_ID = "581CANivore";
 
   public static final int PDP_ID = 0;
-  public static final ModuleType PDP_TYPE =  ModuleType.kCTRE;
+  public static final ModuleType PDP_TYPE = ModuleType.kCTRE;
 
   public static final VisionMode VISION_MODE = VisionMode.ENABLED_UNUSED;
 
@@ -39,99 +37,73 @@ public class Config {
 
   public static final Translation2d SWERVE_FRONT_LEFT_LOCATION = new Translation2d(0.381, 0.381);
   public static final Translation2d SWERVE_FRONT_RIGHT_LOCATION = new Translation2d(0.381, -0.381);
-  public static final Translation2d SWERVE_BACK_LEFT_LOCATION =
- new Translation2d(-0.381, 0.381);
-  public static final Translation2d SWERVE_BACK_RIGHT_LOCATION =
-   new Translation2d(-0.381, -0.381);
+  public static final Translation2d SWERVE_BACK_LEFT_LOCATION = new Translation2d(-0.381, 0.381);
+  public static final Translation2d SWERVE_BACK_RIGHT_LOCATION = new Translation2d(-0.381, -0.381);
 
   public static final int SWERVE_FL_DRIVE_MOTOR_ID = 8;
   public static final int SWERVE_FL_STEER_MOTOR_ID = 9;
   public static final int SWERVE_FL_CANCODER_ID = 13;
   public static final SwerveModuleConstants SWERVE_FL_CONSTANTS =
       new SwerveModuleConstants(
-              Rotation2d.fromDegrees(117.19), SwerveCorner.FRONT_LEFT, false, false);
+          Rotation2d.fromDegrees(117.19), SwerveCorner.FRONT_LEFT, false, false);
   public static final int SWERVE_FR_DRIVE_MOTOR_ID = 6;
   public static final int SWERVE_FR_STEER_MOTOR_ID = 7;
   public static final int SWERVE_FR_CANCODER_ID = 12;
   public static final SwerveModuleConstants SWERVE_FR_CONSTANTS =
       new SwerveModuleConstants(
-              Rotation2d.fromDegrees(32.2), SwerveCorner.FRONT_RIGHT, false, false);
+          Rotation2d.fromDegrees(32.2), SwerveCorner.FRONT_RIGHT, false, false);
   public static final int SWERVE_BL_DRIVE_MOTOR_ID = 4;
   public static final int SWERVE_BL_STEER_MOTOR_ID = 5;
   public static final int SWERVE_BL_CANCODER_ID = 11;
-  public static final SwerveModuleConstants SWERVE_BL_CONSTANTS = new SwerveModuleConstants(
-              Rotation2d.fromDegrees(-101.25), SwerveCorner.BACK_LEFT, false, false);
+  public static final SwerveModuleConstants SWERVE_BL_CONSTANTS =
+      new SwerveModuleConstants(
+          Rotation2d.fromDegrees(-101.25), SwerveCorner.BACK_LEFT, false, false);
   public static final int SWERVE_BR_DRIVE_MOTOR_ID = 2;
   public static final int SWERVE_BR_STEER_MOTOR_ID = 3;
   public static final int SWERVE_BR_CANCODER_ID = 10;
   public static final SwerveModuleConstants SWERVE_BR_CONSTANTS =
-new SwerveModuleConstants(
-              Rotation2d.fromDegrees(-75.42), SwerveCorner.BACK_RIGHT, false, false);
+      new SwerveModuleConstants(
+          Rotation2d.fromDegrees(-75.42), SwerveCorner.BACK_RIGHT, false, false);
   // 104.58
-  public static final int ELEVATOR_MOTOR_ID = 14;
-
-  public static final double ELEVATOR_GEARING =16.0;
-  public static final double ELEVATOR_MIN_HEIGHT =  0;
-  public static final double ELEVATOR_MAX_HEIGHT =   12;
-
-  public static final double ELEVATOR_KV =  0;
-  public static final double ELEVATOR_KF =  0;
-  public static final double ELEVATOR_KP =  0.8;
-  public static final double ELEVATOR_KI =  0;
-  public static final double ELEVATOR_KD =   0;
-  public static final double ELEVATOR_KS =  0;
-
-  public static final int ELEVATOR_CRUISE_VELOCITY =  15000;
-  public static final int ELEVATOR_ACCELERATION =  27500;
-  public static final boolean ELEVATOR_INVERTED =  true;
-
+  
   public static final int LIGHTS_CANDLE_ID = 15;
   public static final int LIGHTS_LED_COUNT = 0;
 
   public static final int WRIST_MOTOR_ID = 16;
-  public static final double WRIST_GEARING = 48.0 * 2;
-  public static final int WRIST_KV =  0;
-  public static final double WRIST_KP = 0.1;
-  public static final int WRIST_KI =  0;
-  public static final int WRIST_KD =  0;
-  public static final int WRIST_MOTION_CRUISE_VELOCITY =  20000;
-  public static final int WRIST_MOTION_ACCELERATION = 50000;
-  public static final double WRIST_HOMED_CURRENT =  5;
-  public static final Rotation2d WRIST_HOMED_ANGLE = Rotation2d.fromDegrees(0.0);
-  public static final double WRIST_HOMING_VOLTAGE =  -0.15;
+  public static final double WRIST_GEARING = 1.0;
+  public static final double WRIST_KP = 0;
+  public static final int WRIST_KI = 0;
+  public static final int WRIST_KD = 0;
+  public static final int WRIST_MOTION_MAX_VELOCITY = 0;
+  public static final int WRIST_MOTION_MAX_ACCELERATION = 0;
 
   public static final int INTAKE_MOTOR_ID = 17;
-  public static final boolean INVERTED_INTAKE =  true;
+  public static final boolean INVERTED_INTAKE = true;
 
-  public static final double SWERVE_STEER_KV =  0.0;
-  public static final double SWERVE_STEER_KP =  3.0;
-  public static final double SWERVE_STEER_KI =  0.0;
-  public static final double SWERVE_STEER_KD =  0.0;
-  public static final double SWERVE_STEER_KS =  0.0;
+  public static final double SWERVE_STEER_KV = 0.0;
+  public static final double SWERVE_STEER_KP = 3.0;
+  public static final double SWERVE_STEER_KI = 0.0;
+  public static final double SWERVE_STEER_KD = 0.0;
+  public static final double SWERVE_STEER_KS = 0.0;
 
-  public static final int SWERVE_DRIVE_VOLTAGE_PEAK_FORWARD_VOLTAGE =  0;
-  public static final int SWERVE_DRIVE_VOLTAGE_PEAK_REVERSE_VOLTAGE =  0;
+  public static final int SWERVE_DRIVE_VOLTAGE_PEAK_FORWARD_VOLTAGE = 0;
+  public static final int SWERVE_DRIVE_VOLTAGE_PEAK_REVERSE_VOLTAGE = 0;
   public static final double SWERVE_DRIVE_CURRENT_LIMIT = 0;
   public static final boolean SWERVE_DRIVE_LIMITS_ENABLE = true;
 
-  public static final double SWERVE_DRIVE_KP =  0.1;
-  public static final double SWERVE_DRIVE_KI =  0.0;
-  public static final double SWERVE_DRIVE_KD =  0.0;
-  public static final double SWERVE_DRIVE_KV =  0.117;
+  public static final double SWERVE_DRIVE_KP = 0.1;
+  public static final double SWERVE_DRIVE_KI = 0.0;
+  public static final double SWERVE_DRIVE_KD = 0.0;
+  public static final double SWERVE_DRIVE_KV = 0.117;
   public static final double SWERVE_DRIVE_KS = 0.0;
 
-  public static final double STEER_MOTOR_LIMITS =  0.0;
-  public static final boolean SWERVE_MOTOR_LIMITS_ENABLED =  true;
+  public static final double STEER_MOTOR_LIMITS = 0.0;
+  public static final boolean SWERVE_MOTOR_LIMITS_ENABLED = true;
   public static final PIDConstants SWERVE_TRANSLATION_PID = new PIDConstants(7, 0, 0.05);
   public static final PIDConstants SWERVE_ROTATION_PID = new PIDConstants(4.25, 0, 0.4);
   public static final PIDConstants SWERVE_ROTATION_SNAP_PID =
-           new PIDConstants(0, 0, 0); // TODO: Edit these PID constants
+      new PIDConstants(0, 0, 0); // TODO: Edit these PID constants
   public static final boolean SWERVE_USE_FOC = true;
-
-  public static final double SUPERSTRUCTURE_COLLISION_HEIGHT =  26;
-  public static final Rotation2d SUPERSTRUCTURE_WRIST_RANGE = Rotation2d.fromDegrees(13);
-
-  public static final double ROBOT_CENTER_TO_FRONT = Units.inchesToMeters(17.5);
 
   private Config() {}
 }
---
+++ src/main/java/frc/robot/wrist/WristSubsystem.java
@@ -0,0 +1,126 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.wrist;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.config.Config;
+import frc.robot.util.HomingState;
+import frc.robot.util.scheduling.LifecycleSubsystem;
+import frc.robot.util.scheduling.SubsystemPriority;
+import org.littletonrobotics.junction.Logger;
+
+public class WristSubsystem extends LifecycleSubsystem {
+  private static final double WRIST_TOLERANCE = 3;
+  private final CANSparkMax motor;
+  private final RelativeEncoder encoder;
+  private final SparkMaxPIDController pid;
+  private final LinearFilter currentFilter = LinearFilter.movingAverage(0);
+  private Rotation2d goalAngle = new Rotation2d();
+  private HomingState homingState = HomingState.NOT_HOMED;
+
+  public WristSubsystem(CANSparkMax motor) {
+    super(SubsystemPriority.WRIST);
+
+    pid = motor.getPIDController();
+    this.motor = motor;
+    encoder = motor.getEncoder();
+    pid.setP(Config.WRIST_KP);
+    pid.setI(Config.WRIST_KI);
+    pid.setD(Config.WRIST_KD);
+    pid.setSmartMotionMaxAccel(Config.WRIST_MOTION_MAX_ACCELERATION, 0);
+    pid.setSmartMotionMaxVelocity(Config.WRIST_MOTION_MAX_VELOCITY, 0);
+    encoder.setPositionConversionFactor(Config.WRIST_GEARING);
+  }
+
+  @Override
+  public void enabledPeriodic() {
+    double rawCurrent = motor.getOutputCurrent();
+    double filteredCurrent = currentFilter.calculate(rawCurrent);
+
+    if (homingState == HomingState.NOT_HOMED) {
+      // set the motor to 0
+      motor.set(0);
+    }
+    if (homingState == HomingState.HOMING) {
+      // set the motor to run at like 10% ish
+      motor.set(0.1);
+
+      if (filteredCurrent > 5) {
+        motor.set(0);
+        encoder.setPosition(0);
+        set(Rotation2d.fromDegrees(0));
+        homingState = HomingState.HOMED;
+      }
+    } else if (homingState == HomingState.HOMED) {
+      // read the goal angle, set the motor to go to that position
+      pid.setReference(goalAngle.getRotations(), ControlType.kSmartMotion);
+    }
+  }
+
+  // when the robot is enabled and homing state is HOMED, go to the stored goal angle
+  public HomingState getHomingState() {
+    return homingState;
+  }
+
+  // when the robot is enabled and homing state is HOMING, run the homing code (run motor at 10%),
+  // stop when current is above 10A
+  public void startHoming() {
+    homingState = HomingState.HOMING;
+  }
+
+  // when the robot is enabled and homing state is NOT_HOMED, set the voltage to 0
+  public void resetHoming() {
+    homingState = HomingState.NOT_HOMED;
+    motor.set(0);
+  }
+
+  // when the robot is powered on, log a few values related to the wrist
+  @Override
+  public void robotPeriodic() {
+    Logger.getInstance().recordOutput("Wrist/Velocity", encoder.getVelocity());
+    Logger.getInstance().recordOutput("Wrist/Angle", getWristAngle().getDegrees());
+    Logger.getInstance().recordOutput("Wrist/GoalAngle", goalAngle.getDegrees());
+    Logger.getInstance().recordOutput("Wrist/DutyCycleOutput", motor.getAppliedOutput());
+    Logger.getInstance().recordOutput("Wrist/StatorCurrent", motor.getOutputCurrent());
+    Logger.getInstance().recordOutput("Wrist/Homing", homingState.toString());
+  }
+
+  // current angle, goal angle
+  private Rotation2d getWristAngle() {
+    return Rotation2d.fromRotations(encoder.getPosition());
+  }
+
+  // add a method to set the goal angle
+  public void set(Rotation2d angle) {
+    goalAngle = angle;
+  }
+
+  // add a method to check if we're at a given angle (within 3 degrees of it)
+  public boolean atAngle(Rotation2d angle) {
+    double encoderAngle = getWristAngle().getDegrees();
+    return Math.abs(encoderAngle - angle.getDegrees()) < WRIST_TOLERANCE;
+  }
+
+  // add a method to return a command which sets the goal angle to something and then exits the
+  // command once at that angle
+  public Command goToAngle(Rotation2d toGoalAngle) {
+    return run(() -> {
+          set(toGoalAngle);
+        })
+        .until(() -> atAngle(goalAngle));
+  }
+
+  public Command getHomeCommand() {
+    return runOnce(() -> startHoming())
+        .andThen(Commands.waitUntil(() -> getHomingState() == HomingState.HOMED));
+  }
+}
